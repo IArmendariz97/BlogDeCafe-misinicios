386a74db31b6fcf010da006754105c4eaa3ce687
Iñaki Armendariz <iniakitoo@gmail.com>
2023-10-18 22:33:16 -0300

    quitando comentarios

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
 // querySelector
 
 // .clase
-// #id 
-const heading = document.querySelector('.header__texto h2')  // retorna 0 o 1 elemento
+// #id
+const heading = document.querySelector(".header__texto h2"); // retorna 0 o 1 elemento
 
-
-heading.textContent = 'Nuevo Heading'; //cambiar texto
-heading.classList.add('nueva-clase'); //agregar clase
+heading.textContent = "Nuevo Heading"; //cambiar texto
+heading.classList.add("nueva-clase"); //agregar clase
 console.log(heading);
 
 // querySelectorAll
 
-const enlaces = document.querySelectorAll('.navegacion a')
-enlaces[0].textContent = 'Nuevo Texto Para Enlace'; 
-enlaces[0].href = 'https://google.com'; // Cambiar hacia adonde te lleva el enlace
+const enlaces = document.querySelectorAll(".navegacion a");
+enlaces[0].textContent = "Nuevo Texto Para Enlace";
+enlaces[0].href = "https://google.com"; // Cambiar hacia adonde te lleva el enlace
 // enlaces[0].classList.remove('navegacion__enlace');
-
-
 
 // getElementById
 
@@ -25,23 +22,24 @@ enlaces[0].href = 'https://google.com'; // Cambiar hacia adonde te lleva el enla
 
 //Generar un nuevo enlace
 
-const nuevoEnlace = document.createElement('A');
+const nuevoEnlace = document.createElement("A");
 
 //Agregar el href
-nuevoEnlace.href = 'nuevo-enlace';
+nuevoEnlace.href = "nuevo-enlace";
 
 //  Agregar el texto
-nuevoEnlace.textContent = 'Tienda virtual';
+nuevoEnlace.textContent = "Tienda virtual";
 
 //Agregar la clase
-nuevoEnlace.classList.add('navegacion__enlace');
+nuevoEnlace.classList.add("navegacion__enlace");
 
-//Agregarlo al documento 
-const navegacion = document.querySelector('.navegacion');
+//Agregarlo al documento
+const navegacion = document.querySelector(".navegacion");
 navegacion.appendChild(nuevoEnlace);
-{/* <a href="nosotros.html" class="nuevo-enlace">Nosotros</a> */}
+{
+  /* <a href="nosotros.html" class="nuevo-enlace">Nosotros</a> */
+}
 
- 
 console.log(nuevoEnlace);
 
 //Generar codigo html
@@ -51,130 +49,124 @@ console.log(nuevoEnlace);
 
 console.log(1);
 
-window.addEventListener('load', imprimir);
-window.onload = function () {
-    console.log(3);
-}
+window.addEventListener("load", imprimir);
+window.onload = function() {
+  console.log(3);
+};
 
-document.addEventListener('DOMContentLoaded', function(){ // Solo espera que se descargue el HTML, no espera css ni imagenes
-    console.log(4);
-})
+document.addEventListener("DOMContentLoaded", function() {
+  // Solo espera que se descargue el HTML, no espera css ni imagenes
+  console.log(4);
+});
 
 //Usualmente se usa este
 
-function imprimir() { //Load espera a que el JS y los archivos que dependen del HTML esten listo
-    console.log(2);
+function imprimir() {
+  //Load espera a que el JS y los archivos que dependen del HTML esten listo
+  console.log(2);
 }
 
 console.log(5);
 
 window.onscroll = function(evento) {
-    console.log(evento);
-}
+  console.log(evento);
+};
 
-// Seleccionar elementos y asociarles un evento 
+// Seleccionar elementos y asociarles un evento
 
 // const btnEnviar = document.querySelector('.boton--primario');
 
 // btnEnviar.addEventListener('click', function(evento) { // click load minusculas
 //     console.log(evento.target);
-//     evento.preventDefault(); 
+//     evento.preventDefault();
 
 //     // Validar un formulario
-
 
 //     console.log('enviando formulario');
 // });
 
-
 //Eventos de los inputs y textarea
 const datos = {
-    nombre : '',
-    email : '',
-    mensaje : ''
-}
+  nombre: "",
+  email: "",
+  mensaje: "",
+};
 
-const nombre = document.querySelector('#nombre');
+const nombre = document.querySelector("#nombre");
 
-const email = document.querySelector('#email');
+const email = document.querySelector("#email");
 
-const mensaje = document.querySelector('#mensaje');
+const mensaje = document.querySelector("#mensaje");
 
-const formulario = document.querySelector('.formulario');
+const formulario = document.querySelector(".formulario");
 
-nombre.addEventListener('input', leerTexto);
+nombre.addEventListener("input", leerTexto);
 
-email.addEventListener('input', leerTexto);
+email.addEventListener("input", leerTexto);
 
-mensaje.addEventListener('input',leerTexto);
+mensaje.addEventListener("input", leerTexto);
 
 // El evento de subtmit
 
-formulario.addEventListener('submit', function(evento){
-    evento.preventDefault();
-   
+formulario.addEventListener("submit", function(evento) {
+  evento.preventDefault();
 
-    //Validar el formulario
-    const { nombre, email, mensaje} = datos;
-    
-    if (nombre === '' || email === '' || mensaje === '') {
-        mostrarAlerta('Todos los campos son obligatorios', true)
-       
-        return; // corta la ejecucion del codigo
-    }   
+  //Validar el formulario
+  const { nombre, email, mensaje } = datos;
 
-    mostrarAlerta('Formulario Enviado con Exito');
-    
-    
+  if (nombre === "" || email === "" || mensaje === "") {
+    mostrarAlerta("Todos los campos son obligatorios", true);
 
-})
+    return; // corta la ejecucion del codigo
+  }
 
-function mostrarAlerta(mensaje, error = null){
-    const alerta = document.createElement('p');
-    alerta.textContent = mensaje;
-    if (error) {
-        alerta.classList.add('error');
-        
-    } else {
-        alerta.classList.add('correcto')
-    }
+  mostrarAlerta("Formulario Enviado con Exito");
+});
 
-    formulario.appendChild(alerta);
+function mostrarAlerta(mensaje, error = null) {
+  const alerta = document.createElement("p");
+  alerta.textContent = mensaje;
+  if (error) {
+    alerta.classList.add("error");
+  } else {
+    alerta.classList.add("correcto");
+  }
 
-    setTimeout(() => {
-        alerta.remove();
-    }, 5000);
+  formulario.appendChild(alerta);
+
+  setTimeout(() => {
+    alerta.remove();
+  }, 5000);
 }
 
 function leerTexto(e) {
-    // console.log(e.target.value);
+  // console.log(e.target.value);
 
-    datos[e.target.id] = e.target.value;
-     
-    // console.log(e.target);
-    
-    console.log(datos);
+  datos[e.target.id] = e.target.value;
+
+  // console.log(e.target);
+
+  console.log(datos);
 }
-// function mostrarError(mensaje) {
-//     const error = document.createElement('P');
-//         error.textContent = mensaje;
-//         error.classList.add('error');
-        
-//         formulario.appendChild(error);
-        
-//         //desaparezca
-//      setTimeout(() => {
-//         error.remove();
-//      }, 5000);
-        
-//     }
-// function mostrarMensaje(mensaje) {
-//     const msj = document.createElement('P');
-//     msj.textContent = mensaje;
-//     msj.classList.add('mensaje');
-//     formulario.appendChild(msj);
+function mostrarError(mensaje) {
+  const error = document.createElement("P");
+  error.textContent = mensaje;
+  error.classList.add("error");
 
-//     setTimeout(() => {
-//        mensaje.remove();
-//     }, 5000);
-// }
+  formulario.appendChild(error);
+
+  //desaparezca
+  setTimeout(() => {
+    error.remove();
+  }, 5000);
+}
+function mostrarMensaje(mensaje) {
+  const msj = document.createElement("P");
+  msj.textContent = mensaje;
+  msj.classList.add("mensaje");
+  formulario.appendChild(msj);
+
+  setTimeout(() => {
+    mensaje.remove();
+  }, 5000);
+}
